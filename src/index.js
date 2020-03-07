13f3d187338af089d56066d76fedb5d928402c4c
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-03-07 13:57:42 +0100

    nested schema

--- src/index.js
+++ src/index.js
@@ -4,17 +4,43 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+// add dummy data
+let links = [{
+  id: 'link-0',
+  url: 'urlno1',
+  description: 'Fullstack tutorial for GraphQL'
+},
+{
+id: 'link-1',
+url: 'urlno2',
+description: 'Fullstack tutorial for GraphQL'
+}]
 
-// implementation of GraphQL schema
+// Resolver function: implementation of GraphQL schema
 const resolvers = {
   Query: {
-    info: () => `unimportant info`
+    info: () => `some query info`,
+    // 2
+    feed: () => links,
+  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
-// 3
+// implement the server
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
