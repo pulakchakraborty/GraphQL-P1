4290fe84127109a589b650e351db1012f25ddc33
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-03-08 00:24:55 +0100

    adding mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// define GraphQL schema
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
 // add dummy data
 let links = [{
   id: 'link-0',
@@ -25,24 +12,40 @@ url: 'urlno2',
 description: 'Fullstack tutorial for GraphQL'
 }]
 
+// generate unique id for newly created `link`elements
+let idCount = links.length
+
 // Resolver function: implementation of GraphQL schema
 const resolvers = {
   Query: {
     info: () => `some query info`,
-    // 2
     feed: () => links,
   },
-  // 3
+  Mutation: {
+    // create a new `link` object, adds it to the existing `links` list, and returns the new `link`
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
+  // remove the link resolvers entirely
+  /*
   Link: {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
     url: (parent) => parent.url,
   }
+  */
 }
 
 // implement the server
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
